5db3887e89cd2c307fb880e8e631b912b42bfb4f
Madhi <madhi.naga@gmail.com>
2023-02-08 17:57:41 -0500

    Error checking for backend

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,7 +1,24 @@
 import { Box, Button } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+
+  const BACKEND_URI = process.env.REACT_APP_FRONTEND_URI || 'http://localhost:8080';
+  const navigate = useNavigate();
+
+  // error check to see if backend is running
+  useEffect(() => {  
+    axios.get(BACKEND_URI)
+    .then((res) => {})
+    .catch((err) => {
+      console.log(err);
+      navigate('/error');
+    });
+  }, [BACKEND_URI, navigate]);
+
+
   return (
     <Box
       display='flex'
--- src/client/src/router.js
+++ src/client/src/router.js
@@ -22,7 +22,11 @@ const router = createBrowserRouter([
     {
         path: '/home',
         element: <HomePage />
-    }
+    },
+    {
+      path: '/error',
+      element: <ErrorPage />,
+  }
 ])
 
 export default router;
