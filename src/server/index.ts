887979e210006c23bc15e5ec2114cbc06803d284
Madhi <madhi.naga@gmail.com>
2023-02-13 17:51:09 -0500

    Setup session endpoints

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,6 +7,7 @@ const user = require('./routes/user');
 const pref = require('./routes/pref');
 const group = require('./routes/group');
 const invite = require('./routes/invite');
+const voteSession = require('./routes/voteSession');
 
 const app: Express = express();
 
@@ -22,5 +23,6 @@ app.use('/api/user', user);
 app.use('/api/user/pref', pref);
 app.use('/api/group', group);
 app.use('/api/invites', invite);
+app.use('/api/voting', voteSession);
 
 module.exports = app;
