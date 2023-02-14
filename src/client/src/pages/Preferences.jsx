b0a9d88f508a0356cecc83d64126ff8c8341f284
Ali Tabar <justalitabar@gmail.com>
2023-02-13 21:53:31 -0500

    Updated Preferences.jsx
    Replaced placeholder with template with checkbox lists

--- src/client/src/pages/Preferences.jsx
+++ src/client/src/pages/Preferences.jsx
@@ -1,62 +1,125 @@
 import {
-    Box,
-    IconButton,
-    List,
-    ListItem,
-    ListItemSecondaryAction,
-    ListItemText,
-    Typography,
-  } from '@mui/material';
-  import React from 'react';
-  import Header from '../components/Header';
-  import { Check, Clear } from '@mui/icons-material';
-  
-  const JoinGroup = () => {
-    return (
-      <Box>
-        <Header />
+  Box,
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
+  Typography,
+  InputLabel,
+  OutlinedInput,
+  FormControl,
+} from "@mui/material";
+import React from "react";
+import Header from "../components/Header";
+import FriendsList from "../components/FriendsList";
+import GroupsList from "../components/GroupsList";
+import { Check, Clear } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+
+const Preferences = () => {
+  // React-Router Navigation
+  const navigate = useNavigate();
+
+  const handleJoinGroupClick = (e) => {
+    e.preventDefault();
+    navigate("/group/join");
+  };
+
+  const handleCreateGroupClick = (e) => {
+    e.preventDefault();
+    navigate("/group/create");
+  };
+
+  return (
+    <Box>
+      <Header />
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        gap="5vh"
+        minHeight="75vh"
+      >
+        {/* TODO: Every button needs a handler */}
+        <Typography variant="h4" component="h4">
+          Preferences
+        </Typography>
+
         <Box
           display="flex"
           justifyContent="center"
           alignItems="center"
-          flexDirection="column"
-          gap="5vh"
-          marginTop="5%"
-          marginBottom="5%"
-          minHeight="75vh">
-          <Typography variant="h4" component="h4">
-            Join Group
-          </Typography>
-          <List
-            sx={{
-              width: '100%',
-              maxWidth: 200,
-              bgcolor: 'background.paper',
-              border: 'solid',
-              borderRadius: '10px',
-            }}>
-            {[0, 1, 2, 3].map((value) => {
-              const labelId = `invite-list-label-${value}`;
-  
-              return (
-                <ListItem key={value} role={undefined} dense>
-                  <ListItemSecondaryAction>
-                    <IconButton edge="end" aria-label="accept">
-                      <Check />
-                    </IconButton>
-                    <IconButton edge="end" aria-label="decline">
-                      <Clear />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-                  <ListItemText id={labelId} primary={`Invite ${value + 1}`} />
-                </ListItem>
-              );
-            })}
-          </List>
+          flexDirection="row"
+          gap="0%"
+        >
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Typography variant="h6" component="h6">
+              Friends
+            </Typography>
+
+            <Box
+              display="flex"
+              gap="1vh"
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+              padding="20px"
+              borderRadius="10px"
+            >
+              <FriendsList />
+            </Box>
+          </Box>
+
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Typography variant="h6" component="h6">
+              Groups
+            </Typography>
+
+            <Box
+              display="flex"
+              gap="1vh"
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+              padding="20px"
+              borderRadius="10px"
+            >
+              <GroupsList />
+            </Box>
+          </Box>
+        </Box>
+
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="row"
+          gap="2%"
+        >
+          <Button variant="outlined" size="large">
+            Cancel
+          </Button>
+
+          <Button variant="outlined" size="large">
+            Save
+          </Button>
         </Box>
       </Box>
-    );
-  };
-  
-  export default JoinGroup;
-  
+    </Box>
+  );
+};
+
+export default Preferences;
