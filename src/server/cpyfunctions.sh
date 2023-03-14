8323fe9a291e44471530f0340223ae84ba02e3d2
Madhi <madhi.naga@gmail.com>
2023-03-14 11:24:27 -0400

    Added bash script to copy to deploy folder

---
+++ src/server/cpyfunctions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r ./services ./functions/src/services
+cp -r ./routes ./functions/src/routes
+cp -r ./models ./functions/src/models
+cp -r ./api-json ./functions/src/api-json
+cp -r ./static ./functions/src/static
+cp -r ./controllers ./functions/src/controller
