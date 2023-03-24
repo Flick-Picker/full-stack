6d853cdbfa62cf97a9cac6d6468ca038e566af98
Jarrod Colwell <colwellj@mcmaster.ca>
2023-03-23 22:56:50 -0400

    Updated parsing to include more information for the algorithm

--- src/server/generate/parseMovies.ts
+++ src/server/generate/parseMovies.ts
@@ -3,6 +3,8 @@ import movieList from '../api-json/movie_batch.json';
 import MovieObject from '../classes/movieObject';
 import movieGenres from '../api-json/movie_genres.json';
 
+const Movie = { runtime: 130 };
+
 function parseMovieGenres(genreIDs: number[]) {
   const genres = new Set<string>();
   genreIDs.forEach((genreID) => {
@@ -23,6 +25,8 @@ function parseMovieGenres(genreIDs: number[]) {
 export const getMovies = async () => {
   const movies: MovieObject[] = [];
   movieList.forEach((movie) => {
+    // Everything should have a release date so the '2009-' should never be used
+    const releasedate = movie.release_date ? movie.release_date : '2009-';
     const imgURL = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`;
     const genres = parseMovieGenres(movie.genre_ids);
     movies.push(new MovieObject(
@@ -32,7 +36,8 @@ export const getMovies = async () => {
       movie.vote_average,
       movie.vote_count,
       movie.popularity,
-      movie.release_date,
+      Number.parseInt(releasedate.substring(0, releasedate.indexOf('-')), 10),
+      Movie.runtime,
     ));
   });
 
